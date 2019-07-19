d7ceea5739f1f857b8a051fc2385fd647f79db74
Brittany Javalera <brittjavalera@gmail.com>
2019-07-19 22:06:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    is_valid
+    if @side1 == @side2 && @side2== @side3
+      :equilateral
+      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  def is_valid
+  triangles = [(side1 + side2 > side3), (side2 + side3 > side1), (side1 + side3 > side2)]
+  sides = [side1, side2, side3]
+    sides.each do |side| 
+      triangles << false if side <= 0
+  end
+  raise TriangleError if triangles.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
 end
