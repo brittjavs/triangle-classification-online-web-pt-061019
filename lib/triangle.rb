b8585e5832b642a9ac5c7809b217c0e2d7e3fc5e
Brittany Javalera <brittjavalera@gmail.com>
2019-07-19 22:11:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,25 +6,42 @@ class Triangle
     @side3 = side3
   end
 
-  def kind
-    is_valid
-    if @side1 == @side2 && @side2== @side3
-      :equilateral
-      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
-      :isosceles
+def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
     else
-      :scalene
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
   end
+end
+
+  # def kind
+  #   is_valid
+  #   if @side1 == @side2 && @side2== @side3
+  #     :equilateral
+  #     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+  #     :isosceles
+  #   else
+  #     :scalene
+  #   end
+  # end
   
-  def is_valid
-  triangles = [(side1 + side2 > side3), (side2 + side3 > side1), (side1 + side3 > side2)]
-  sides = [side1, side2, side3]
-    sides.each do |side| 
-      triangles << false if side <= 0
-  end
-  raise TriangleError if triangles.include?(false)
-  end
+  # def is_valid
+  # triangles = [(side1 + side2 > side3), (side2 + side3 > side1), (side1 + side3 > side2)]
+  # sides = [side1, side2, side3]
+  #   sides.each do |side| 
+  #     triangles << false if side <= 0
+  # end
+  # raise TriangleError if triangles.include?(false)
+  # end
 
   class TriangleError < StandardError
   end
